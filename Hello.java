78685bc1a368d6d94f932b750efba7cfb4e75438
KENNETH BRYAN GERABAS ESCALA <69161486+asazin90143@users.noreply.github.com>
2023-08-30 21:56:43 +0800

    Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
 Public class {
   public static void main (Stirng [] args){
-    System.out.println("Kello, Software Engineering 1!")
-    System.out.println("Student Name: Escala, Kenneth Bryan G.")
+    System.out.println("Complicated world of SoftDev that is why it pays much!");
       }
 }
