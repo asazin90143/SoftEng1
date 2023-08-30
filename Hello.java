b5011c744df5a6dc7466d8fb8975a1e6c10362e7
KENNETH BRYAN GERABAS ESCALA <69161486+asazin90143@users.noreply.github.com>
2023-08-30 22:01:23 +0800

    Modify Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 Public class {
   public static void main (Stirng [] args){
-    System.out.println("Complicated world of SoftDev that is why it pays much!");
+    System.out.println("Complicated world of SoftDev that is why it pays much!"); 
       }
 }
